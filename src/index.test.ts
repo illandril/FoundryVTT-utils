d5f1ae6e45bbbcacdb06fd071be36894b6ed7228
Joe Spandrusyszyn <github@illandril.net>
2022-12-17 22:16:59 -0500

    Export Manifest

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,13 @@
+import * as Manifest from './Manifest';
 import Module from './Module';
 import * as index from './index';
 
+jest.mock('./Manifest');
 jest.mock('./Module');
+
+it('exports Manifest', () => {
+  expect(index.Manifest).toBe(Manifest);
+});
 
 it('exports Module', () => {
   expect(index.Module).toBe(Module);
@@ -9,6 +15,7 @@ it('exports Module', () => {
 
 it('exports only intended values', () => {
   const expectedKeys: string[] = [
+    'Manifest',
     'Module',
   ];
   const keys = Object.keys(index);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { default as Module } from './Module';
+export * as Manifest from './Manifest';
